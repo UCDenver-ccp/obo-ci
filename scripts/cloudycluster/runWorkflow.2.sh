af82ed56d5e9fc8562282400e7b3bbe1619f60c1
bill-baumgartner <bill.baumgartner@gmail.com>
2018-04-04 11:31:30 -0600

    Squash - walltime = 1hr

--- scripts/cloudycluster/runWorkflow.2.sh
+++ scripts/cloudycluster/runWorkflow.2.sh
@@ -4,20 +4,16 @@
 # May not want to use Spot for this one to make sure it doesn't get killed?
 #CC -it r4.large
 #CC -sp 0.08
-#CC -tl 0:0:30
+#CC -tl 0:1:0
 #CC -ni 1
 
 #SBATCH -N 1
 
 # Set the path to the shared filesystem where the data will be stored and persisted across runs
 export SHARED_FS=/mnt/efsdata
-export WORK_DIRECTORY=${SHARED_FS}/obo-ci-data
-export CODE_BASE_DIRECTORY=${SHARED_FS}/obo-ci.git
 export JOB_LOGS_DIRECTORY=${SHARED_FS}/job-logs
-export S3_PATH=s3://cc.obo-ci
-export LOG_FILE=${JOB_LOGS_DIRECTORY}/workflow.log
 
-touch ${JOB_LOGS_DIRECTORY}/workflow_begin.txt
+touch ${JOB_LOGS_DIRECTORY}/workflow_2_begin.txt
 
 echo "PATH: "
 echo $PATH
@@ -28,6 +24,6 @@ python -V
 echo "JAVA VERSION: "
 java -version
 
-touch ${JOB_LOGS_DIRECTORY}/workflow_end.txt
+touch ${JOB_LOGS_DIRECTORY}/workflow_2_end.txt
 
 
